0d7e546a473e3a46a5a805d37768e128aa4576d7
William You <williamwii3189@gmail.com>
2014-05-04 01:14:19 -0700

    cs416/a4

---
+++ cs416/ass4/zookeeper-leader/src/main/java/net/spike/zookeeper/NodeMonitor.java
@@ -0,0 +1,166 @@
+package net.spike.zookeeper;
+
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.apache.zookeeper.CreateMode.PERSISTENT;
+
+/**
+ * User: cyberroadie
+ * Date: 21/11/2011
+ */
+public class NodeMonitor implements Watcher, AsyncCallback.ChildrenCallback {
+
+    final Logger logger = LoggerFactory.getLogger(NodeMonitor.class);
+
+    private ZooKeeper zooKeeper;
+    private long sequenceNumber;
+    private long processNumber;
+    private static final String ROOT = "/ELECTION";
+    private NodeMonitorListener listener = null;
+    private String znode;
+
+    /**
+     * Start method to give the listener a change to set itself so
+     * as to receive all messages
+     * @throws IOException
+     */
+    public void start() throws IOException {
+        this.zooKeeper = new ZooKeeper("localhost:2181", 3000, this);
+    }
+
+    public void setZooKeeper(ZooKeeper zooKeeper) {
+        this.zooKeeper = zooKeeper;
+    }
+
+    public void setListener(NodeMonitorListener monitor) {
+        this.listener = monitor;
+        this.znode = ROOT + "/" + listener.getProcessName();
+    }
+
+    @Override
+    public void processResult(int rc, String path, Object o, List<String> children) {
+        logger.debug(listener.getProcessName() + "Children callback: " +
+                listener.getProcessName() + ":" + children.toString());
+        if(rc == KeeperException.Code.Ok) {
+            if(getLowestNumber(children) == processNumber)
+                listener.startSpeaking();
+            else
+                listener.stopSpeaking();
+        } else {
+            listener.stopSpeaking();
+        }
+    }
+
+    public long getLowestNumber(List<String> children) {
+        return getLargestProcessNumber(children);
+
+        // long lowest = sequenceNumber;
+        // for (String child : children) {
+        //     long current = parseSequenceNumber(child);
+        //     if(current < lowest) lowest = current;
+        // }
+        // return lowest;
+    }
+
+    public long getLargestProcessNumber(List<String> children) {
+        long largest = processNumber;
+        for (String child : children) {
+            long current = parseProcessNumber(child);
+            if(largest < current) largest = current;
+        }
+        return largest;
+    }
+
+    public long parseSequenceNumber(String znode) {
+        return Integer.parseInt(znode.substring(znode.lastIndexOf(".") + 1));
+    }
+
+    public long parseProcessNumber(String znode) {
+        return Integer.parseInt(znode.substring(znode.lastIndexOf("-") + 1, znode.lastIndexOf(".")));
+    }
+
+    public interface NodeMonitorListener {
+        public void startSpeaking();
+
+        public void stopSpeaking();
+
+        public String getProcessName();
+
+    }
+
+    @Override
+    public void process(WatchedEvent event) {
+        logger.debug(listener.getProcessName() + "Processing event for znode " + event.getPath());
+        if (event.getType() == Watcher.Event.EventType.None) {
+            processNoneEvent(event);
+        } else {
+            String path = event.getPath();
+            if (path != null && path.equals(ROOT)) {
+                logger.debug(listener.getProcessName() + "Something changed on root node");
+                zooKeeper.getChildren(ROOT, true, this, null);
+            }
+        }
+    }
+
+    public void processNoneEvent(WatchedEvent event) {
+        switch (event.getState()) {
+            case SyncConnected:
+                logger.info(listener.getProcessName() + " is connected to Zookeeper");
+                try {
+                    createRootIfNotExists();
+                    logger.debug(listener.getProcessName() + ": Putting a watch on " + ROOT);
+                    zooKeeper.getChildren(ROOT, true, this, null);
+                    processNumber = createZnode();
+                } catch (InterruptedException e) {
+                    logger.error("Interrupted after connection", e);
+                    System.exit(1);
+                } catch (KeeperException e) {
+                    logger.error("Interrupted after connection", e);
+                    System.exit(1);
+                }
+                break;
+            case Disconnected:
+                logger.debug("Disconnected");
+                listener.stopSpeaking();
+                break;
+            case Expired:
+                logger.debug("Expired");
+                listener.stopSpeaking();
+                break;
+        }
+    }
+
+    public long createZnode() throws InterruptedException, KeeperException {
+        long processNumber = ((int)Math.floor(Math.random() * 999)) + 1;
+        String znode = this.znode.substring(0, this.znode.lastIndexOf("-") + 1) + String.format("%03d", processNumber) + this.znode.substring(this.znode.lastIndexOf("."));
+        this.znode = zooKeeper.create(znode, new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+
+        logger.debug("Created znode: " + this.znode);
+        return parseProcessNumber(this.znode);
+    }
+
+    public void createRootIfNotExists() throws InterruptedException, KeeperException {
+        Stat stat = zooKeeper.exists(ROOT, false);
+        if (stat == null) {
+            try {
+                zooKeeper.create(ROOT, new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, PERSISTENT);
+            } catch (KeeperException.NodeExistsException ex) {
+                // If znode gets created in between exists and create by another client ignore error
+            }
+        }
+    }
+
+    public void setSequenceNumber(long sequenceNumber) {
+        this.sequenceNumber = sequenceNumber;
+    }
+
+    public void setProcessNumber(long processNumber) {
+        this.processNumber = processNumber;
+    }
+}
---
+++ cs416/ass4/zookeeper-leader/src/test/java/net/spike/zookeeper/NodeMonitorTest.java
@@ -0,0 +1,83 @@
+package net.spike.zookeeper;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnit4Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+import static org.apache.zookeeper.CreateMode.PERSISTENT;
+
+/**
+ * User: cyberroadie
+ * Date: 24/11/2011
+ */
+@RunWith(JMock.class)
+public class NodeMonitorTest {
+
+    Mockery context = new JUnit4Mockery() {{
+        // Concrete class zookeeper needs to be mocked
+        setImposteriser(ClassImposteriser.INSTANCE);
+    }};
+
+    NodeMonitor classToTest;
+
+    @Before
+    public void setup() throws IOException, InterruptedException, KeeperException {
+        classToTest = new NodeMonitor();
+    }
+
+    @Test
+    public void testParseSequenceNumber() throws Exception {
+        assertEquals(154, classToTest.parseSequenceNumber("/ELECTION/pid-70878.0000000154"));
+    }
+
+    @Test
+    public void testGetLowestNumber() throws IOException, InterruptedException, KeeperException {
+        classToTest.setProcessNumber(163);
+        List<String> children = new ArrayList<String>();
+        children.add("/ELECTION/pid-233.000003");
+        children.add("/ELECTION/pid-163.000015");
+        children.add("/ELECTION/pid-12333.000333");
+        children.add("/ELECTION/pid-003.000033");
+        long expected = 12333;
+        long result = classToTest.getLowestNumber(children);
+        assertEquals(expected, result);
+    }
+
+    /**
+     * NodeMonitor.createRootIfNotExists() should catch NodeExists. In the case
+     * of multiple clients, this is a possibility.
+     * @throws InterruptedException
+     * @throws KeeperException
+     */
+    @Test
+    public void testCreateRootIfNotExistsWithNodeExistsException() throws InterruptedException, KeeperException {
+        final ZooKeeper zookeeper = context.mock(ZooKeeper.class);
+        classToTest = new NodeMonitor();
+        classToTest.setZooKeeper(zookeeper);
+
+        // expectations
+        context.checking(new Expectations() {{
+            oneOf(zookeeper).exists("/ELECTION", false);
+            will(returnValue(null));
+            oneOf(zookeeper).create("/ELECTION", new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, PERSISTENT);
+            will(throwException(new KeeperException.NodeExistsException()));
+        }});
+
+        classToTest.createRootIfNotExists();
+
+    }
+
+}
